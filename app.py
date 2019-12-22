880075e626d477bb2bfd9930e2680d860de4ccd9
Vladislav <bolilyivs@gmail.com>
2019-12-22 21:15:28 +0500

    For Front-end

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, send_from_directory
 from flask_cors import CORS
 from flask_httpauth import HTTPBasicAuth
 from queries.repository import *
@@ -27,21 +27,25 @@ def verify_password(username, password):
 
 
 @app.route("/", methods=['GET'])
-@auth.login_required
-def test():
-    return jsonify("Hello, %s!" % auth.username())
+def index():
+    return render_template('index.html')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
 
 #####################################
 ##  Task
 #####################################
-@app.route("/repository/check", methods=['POST'])
+@app.route("/api/repository/check", methods=['POST'])
 @auth.login_required
 def check_repository():
     name = request.get_json()
     return jsonify(check_repository_query(name["name"]))
 
 
-@app.route("/repository", methods=['GET'])
+@app.route("/api/repository", methods=['GET'])
 @auth.login_required
 def get_repository_list():
     offset = request.args.get("offset", default=0, type = int)
@@ -51,29 +55,29 @@ def get_repository_list():
         raise -1
     return jsonify(data)
 
-@app.route("/repository/my", methods=['GET'])
+@app.route("/api/repository/my", methods=['GET'])
 @auth.login_required
 def get_my_repository_list():
     offset = request.args.get("offset", default=0, type = int)
     limit = request.args.get("limit", default=15, type = int)
     return jsonify(get_repository_list_query(offset, limit, auth.username(), my=True))
 
-@app.route("/repository/count", methods=['GET'])
+@app.route("/api/repository/count", methods=['GET'])
 @auth.login_required
 def get_repository_count():
     return jsonify(get_repository_count_query())
 
-@app.route("/repository/my/count", methods=['GET'])
+@app.route("/api/repository/my/count", methods=['GET'])
 @auth.login_required
 def get_my_repository_count():
     return jsonify(get_repository_count_query(auth.username()))
 
-@app.route("/repository/<int:repository_id>", methods=['GET'])
+@app.route("/api/repository/<int:repository_id>", methods=['GET'])
 @auth.login_required
 def get_repository(repository_id):
     return jsonify(get_repository_query(repository_id))
 
-@app.route("/repository/create", methods=['POST'])
+@app.route("/api/repository/create", methods=['POST'])
 @auth.login_required
 def create_repository():
     repository = request.get_json()
@@ -98,7 +102,7 @@ def create_repository():
     create_repository_query(repository, auth.username())
     return jsonify("ok")
 
-@app.route("/repository/<int:repository_id>/update", methods=['PUT'])
+@app.route("/api/repository/<int:repository_id>/update", methods=['PUT'])
 @auth.login_required
 def update_repository(repository_id):
     repository = request.get_json()
@@ -122,19 +126,19 @@ def update_repository(repository_id):
         return jsonify("ok")
     return jsonify(code)
 
-@app.route("/repository/<int:repository_id>/delete", methods=['DELETE'])
+@app.route("/api/repository/<int:repository_id>/delete", methods=['DELETE'])
 @auth.login_required
 def delete_repository(repository_id):
     delete_repository_query(repository_id, auth.username())
     return jsonify("ok")
 
-@app.route("/repository/<int:repository_id>/reset", methods=['get'])
+@app.route("/api/repository/<int:repository_id>/reset", methods=['get'])
 @auth.login_required
 def reset_repository(repository_id):
     reset_repository_query(repository_id, auth.username())
     return jsonify("ok")
 
-@app.route("/repository/<int:repository_id>/run", methods=['get'])
+@app.route("/api/repository/<int:repository_id>/run", methods=['get'])
 @auth.login_required
 def run_task(repository_id):
     run_repository_query(repository_id, auth.username())
@@ -144,18 +148,18 @@ def run_task(repository_id):
 ##  User
 #####################################
 
-@app.route("/user/check", methods=['POST'])
+@app.route("/api/user/check", methods=['POST'])
 @auth.login_required
 def check_user():
     name = request.get_json()
     return jsonify(check_user_query(name["name"]))
 
-@app.route("/user/count", methods=['GET'])
+@app.route("/api/user/count", methods=['GET'])
 @auth.login_required
 def get_user_count():
     return jsonify(get_user_count_query())
 
-@app.route("/user", methods=['GET'])
+@app.route("/api/user", methods=['GET'])
 @auth.login_required
 def get_user_list():
     offset = request.args.get("offset", default=0, type = int)
@@ -165,12 +169,12 @@ def get_user_list():
         raise -1
     return jsonify(data)
 
-@app.route("/user/<int:user_id>", methods=['GET'])
+@app.route("/api/user/<int:user_id>", methods=['GET'])
 @auth.login_required
 def get_user(user_id):
     return jsonify(get_user_query(user_id))
 
-@app.route("/user/create", methods=['POST'])
+@app.route("/api/user/create", methods=['POST'])
 @auth.login_required
 def create_user():
     user = request.get_json()
@@ -180,7 +184,7 @@ def create_user():
     create_user_query(user, auth.username())
     return jsonify("ok")
 
-@app.route("/user/<int:user_id>/update", methods=['PUT'])
+@app.route("/api/user/<int:user_id>/update", methods=['PUT'])
 @auth.login_required
 def update_user(user_id):
     user = request.get_json()
@@ -189,13 +193,13 @@ def update_user(user_id):
         return jsonify("ok")
     return jsonify(code)
 
-@app.route("/user/<int:user_id>/delete", methods=['DELETE'])
+@app.route("/api/user/<int:user_id>/delete", methods=['DELETE'])
 @auth.login_required
 def delete_user(user_id):
     delete_user_query(user_id, auth.username())
     return jsonify("ok")
 
-@app.route("/user/check_group", methods=['GET'])
+@app.route("/api/user/check_group", methods=['GET'])
 @auth.login_required
 def get_group():
     return jsonify(get_group_query(auth.username()))
@@ -205,19 +209,19 @@ def get_group():
 ##  Log
 #####################################
 
-@app.route("/task/count", methods=['GET'])
+@app.route("/api/task/count", methods=['GET'])
 @auth.login_required
 def get_task_count():
     return jsonify(get_task_count_query())
 
-@app.route("/task", methods=['GET'])
+@app.route("/api/task", methods=['GET'])
 @auth.login_required
 def get_task_list():
     offset = request.args.get("offset", default=0, type = int)
     limit = request.args.get("limit", default=50, type = int)
     return jsonify(get_task_list_query(offset, limit))
 
-@app.route("/task/<int:task_id>", methods=['GET'])
+@app.route("/api/task/<int:task_id>", methods=['GET'])
 @auth.login_required
 def get_task(task_id):
     return jsonify(get_task_query(task_id))
@@ -226,7 +230,7 @@ def get_task(task_id):
 ##  zpool
 #####################################
 
-@app.route("/zpool", methods=['GET'])
+@app.route("/api/zpool", methods=['GET'])
 @auth.login_required
 def get_zpool_list_resp():
     if config.isWork:
